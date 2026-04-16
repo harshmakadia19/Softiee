21eeeaa25ef9dc807bb8c7d45a673e258ec08c78
Harsh Makadia <harshmakadia9196@gmail.com>
2026-04-16 00:01:52 -0400

    refactor: update model_loader to register full schema metadata

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,11 +1,18 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_item, review, order_promo, customer, ingredient, item_ingredient, menu_item, payment, promo_code
 
 from ..dependencies.database import engine
 
 
 def index():
     orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
+    customer.Base.metadata.create_all(engine)
+    ingredient.Base.metadata.create_all(engine)
+    item_ingredient.Base.metadata.create_all(engine)
+    menu_item.Base.metadata.create_all(engine)
+    order_item.Base.metadata.create_all(engine)
+    order_promo.Base.metadata.create_all(engine)
+    payment.Base.metadata.create_all(engine)
+    promo_code.Base.metadata.create_all(engine)
+    review.Base.metadata.create_all(engine)
+
+
