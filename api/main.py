99f2e0272be10707d205186eb354628a9477182c
Harsh Makadia <harshmakadia9196@gmail.com>
2026-04-30 18:13:53 -0400

    feat: complete customer management, order tracking, and analytics integration for stories #5, #9, #18, and #20

--- api/main.py
+++ api/main.py
@@ -1,11 +1,16 @@
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from .routers import index as indexRoute
 from .models import model_loader
 from .dependencies.config import conf
 
 
-app = FastAPI()
+app = FastAPI(
+    title="OROS: Online Restaurant Ordering System API",
+    description="Full API documentation for the Online Restaurant Ordering System.",
+    version="1.0.0"
+)
 
 origins = ["*"]
 
@@ -18,7 +23,7 @@ app.add_middleware(
 )
 
 model_loader.index()
-# indexRoute.load_routes(app)
+indexRoute.load_routes(app)
 
 
 if __name__ == "__main__":
