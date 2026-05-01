ce0f3c58bc6334695dab8ac3d56346005deb5466
Tanishka Shankar Sonawane <tsonawa1@charlotte.edu>
2026-04-30 20:15:19 -0400

    feat: implement customer rating and review system for story #13

---
+++ api/routers/reviews.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, status, Query
+from sqlalchemy.orm import Session
+from ..controllers import reviews as controller
+from ..schemas import review as schema
+from ..dependencies.database import get_db
+
+router = APIRouter(tags=['Reviews'], prefix="/reviews")
+
+@router.post("/", response_model=schema.Review)
+def create(request: schema.ReviewCreate, db: Session = Depends(get_db)):
+    return controller.create(db=db, request=request)
+
+@router.get("/", response_model=list[schema.Review])
+def read_all(menu_item_id: int = Query(None), db: Session = Depends(get_db)):
+    return controller.read_all(db, menu_item_id)
+
+@router.delete("/{review_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(review_id: int, db: Session = Depends(get_db)):
+    return controller.delete(db, review_id)
